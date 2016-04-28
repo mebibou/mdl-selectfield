0f92bb536115e14f61c0aeef352de740a46e6e76
Guillaume Royer <guillaumeroyer.mail@gmail.com>
2016-04-28 09:08:44 +0800

    chore(dist): move dist files under dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,36 +13,59 @@
       uglify = require('gulp-uglifyjs'),
       jsFiles = ['src/**/*.js'];
 
-  gulp.task('sass', function () {
+  gulp.task('sass-compress', function() {
     gulp
       .src(sassFiles)
       .pipe(concat('file.scss'))
       .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError))
       .pipe(rename(function(path) {
         path.dirname = '';
         path.basename = 'mdl-selectfield';
         path.extname = '.min.css';
       }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest('./dist'));
   });
 
-  gulp.task('uglify', function() {
+  gulp.task('sass', function () {
+    gulp
+      .src(sassFiles)
+      .pipe(concat('mdl-selectfield.scss'))
+      .pipe(sass({
+        sourceComments: false
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('./dist'));
+  });
+
+  gulp.task('js-compress', function() {
     gulp
       .src(jsFiles)
       .pipe(uglify('mdl-selectfield.min.js', {
         outSourceMap: true
       }))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest('./dist'));
+  });
+
+  gulp.task('js', function() {
+    gulp
+      .src(jsFiles)
+      .pipe(rename(function(path) {
+        path.dirname = '';
+        path.basename = 'mdl-selectfield';
+        path.extname = '.js';
+      }))
+      .pipe(gulp.dest('./dist'));
   });
 
   gulp.task('watch', function () {
-    gulp.watch(sassFiles, ['sass']);
-    gulp.watch(jsFiles, ['uglify']);
+    gulp.watch(sassFiles, ['sass', 'sass-compress']);
+    gulp.watch(jsFiles, ['js', 'js-compress']);
   });
 
-  gulp.task('build', ['sass', 'uglify']);
+  gulp.task('build', ['sass', 'sass-compress', 'js', 'js-compress']);
 
   // The default task (called when you run `gulp` from cli)
   gulp.task('default', ['watch']);
